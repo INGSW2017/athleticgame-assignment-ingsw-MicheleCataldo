4984e2fd9d63fa07009761a9f9cefb467a3c8798
MikeUgo <MikeUgo@LAPTOP-9TTNBEUP>
2017-11-08 18:18:25 +0100

    fine esercizio

--- AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameTestSuite.java
+++ AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameTestSuite.java
@@ -3,16 +3,16 @@ package it.unical.test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-//prima Winners, poi Basic.
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-	   AthleticGameWinnersTest.class,AthleticGameBasicTest.class
-	})
-
 //prima Basic, poi Winners.
 /*@RunWith(Suite.class)
 @Suite.SuiteClasses({
 	   AthleticGameBasicTest.class,AthleticGameWinnersTest.class
 	})*/
+
+//prima Winners, poi Basic.
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	   AthleticGameWinnersTest.class,AthleticGameBasicTest.class
+	})
 
 public class AthleticGameTestSuite {}
